f7abd026287e4a68b94f79337f777b4855bf0a17
Luis Solano <luis_sullivan@proton.me>
2026-03-14 17:01:58 -0600

    add pnpm + abbr for dotfiles

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,18 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# pnpm
+set -gx PNPM_HOME "/Users/luis.solano/Library/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+    set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
+
+abbr --erase dst dss dcd dps # Clean if exists
+abbr dst 'dot status'
+abbr dss 'dot status --short'
+abbr dcd 'dot commit -m'
+abbr dps 'dot push'
+abbr da 'dot add'
+abbr dd 'dot diff'
