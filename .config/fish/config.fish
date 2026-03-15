58dba78114bc25bcb992b962424f946894d8e5ea
Luis Solano <luis_sullivan@proton.me>
2026-03-15 08:34:19 +0000

    remove pnpm leftovers

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,13 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-# pnpm
-set -gx PNPM_HOME "/Users/luis.solano/Library/pnpm"
-if not string match -q -- $PNPM_HOME $PATH
-    set -gx PATH "$PNPM_HOME" $PATH
-end
-# pnpm end
-
 abbr --erase dst dss dcd dps # Clean if exists
 abbr dst 'dot status'
 abbr dss 'dot status --short'
